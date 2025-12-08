8a3563623bce8ece92dd09d7417e7225f30c82ff
Thomas <bourchisthomas@gmail.com>
2025-12-08 13:32:10 +0100

    SignUp backend users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+/* ---ADD Security - max 10 request by ID each 15 min --- 
+
+const rateLimit = require('express-rate-limit');
+const signupLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: { result: false, error: 'Too many signup attempts, please try again later.' }
+});
+*/
+
+router.post('/signup', (req, res) => {
+
+  const { lastName, firstName, email, password, role, establishment_ref } = req.body;
+
+  if (!checkBody(req.body, ['lastName', 'firstName', 'email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check the length of password
+  if (password.length < 6) {
+    return res.json({ result: false, error: 'Password must be at least 6 characters' });
+  }
+
+  // Check if the user has not already been registered by this email 
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        lastName,
+        firstName,
+        email,
+        password: hash,
+        token: uid2(32),
+        createdAt: Date.now(),
+        role: role || 'civil',
+        establishment_ref: establishment_ref || null,
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token, newDoc });
+      });
+    } else {
+      res.json({ result: false, error: 'This Email is already used by an User' });
+    }
+  });
 });
 
+
+
+
 module.exports = router;
+
+
